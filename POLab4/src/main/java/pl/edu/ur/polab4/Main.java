34ae41295c6bb00eaefd689b73bb72dbe8b15db5
Kamil Machański <>
2018-03-21 17:25:47 +0100

    stworzenie obiektów klasy Student (zad2)

--- POLab4/src/main/java/pl/edu/ur/polab4/Main.java
+++ POLab4/src/main/java/pl/edu/ur/polab4/Main.java
@@ -1,5 +1,7 @@
 package pl.edu.ur.polab4;
 
+import java.util.Scanner;
+
 public class Main {
 
     /**
@@ -7,6 +9,30 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO zad 4, 5
+        
+        /*
+        Student stud1 = new Student("Karol", "Kowalski", 21);
+        stud1.toString();
+        
+        Student stud2 = new Student("Jan", nazwisko, 0)
+        */
+        
+        
+        // zad2
+        Student s1 = new Student("Jan", "Matura");
+        Student s2 = new Student(43);
+        Student s3 = new Student("budowa maszyn");
+        Student s4 = new Student(1, 43);
+        
+        s1.pokarzDane();
+        s2.pokarzDane();
+        s3.pokarzDane();
+        s4.pokarzDane();
+        
+        // zad3 
+        Scanner input = new Scanner(System.in);
+        
+        
     }
     
 }
